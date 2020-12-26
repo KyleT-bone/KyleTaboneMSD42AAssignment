839356f371cb2f80875e7d25d4502542f1f8c0fe
KyleT-bone <kyle.tabone.e21475@mcast.edu.mt>
2020-12-26 13:13:40 +0100

    Game Created
    Game is created, set up and added scrolling background

---
+++ 2DCarGame/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+    
+    // The material from the texture
+    Material myMaterial;
+
+    // The movement
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the material of the background
+        myMaterial = GetComponent<Renderer>().material;
+        // Scroll in the y-axis
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Move the material
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
